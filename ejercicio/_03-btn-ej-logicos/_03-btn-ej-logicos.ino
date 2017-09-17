a0bebc6c2e09c12db5e2e320c1be5e527fe348ce
Marani Matias Ezequiel <maranimatias@gmail.com>
2017-09-16 21:11:34 -0300

    fix on samples

--- ejercicio/_03-btn-ej-logicos/_03-btn-ej-logicos.ino
+++ ejercicio/_03-btn-ej-logicos/_03-btn-ej-logicos.ino
@@ -3,19 +3,19 @@ int const btn_1 = 9;
 int const btn_2 = 8;
 
 void setup() {
-  pinMode(led,OUTPUT);
-  pinMode(btn_1,INPUT);
-  pinMode(btn_2,INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(btn_1, INPUT);
+  pinMode(btn_2, INPUT);
 }
 
 void loop() {
-  int short btn1 = digitalRead(btn_1);
-  int short btn2 = digitalRead(btn_2);
+  boolean btn1 = digitalRead(btn_1);
+  boolean btn2 = digitalRead(btn_2);
 
   // Serrie
-  if(btn1 == HIGH && btn2 == HIGH) {
-    digitalWrite(led,HIGH);
+  if (btn1 == HIGH && btn2 == HIGH) {
+    digitalWrite(led, HIGH);
   } else {
-    digitalWrite(led,LOW);
+    digitalWrite(led, LOW);
   }
 }
